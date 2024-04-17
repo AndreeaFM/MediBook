1e8244507711e27a4ea3ed8c9288c2722c174d90
AndreeaFM <deea.fruja@yahoo.com>
2024-04-18 02:51:17 +0300

    Login

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -88,7 +88,7 @@ const Header = () => {
             </div>
 
             <Link to="/login">
-              <button className="bg-[#81BDEE] py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
                 Login
               </button>
             </Link>
