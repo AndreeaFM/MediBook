3660be37334b1edb58d6b4abc57ded24a6ab6402
AndreeaFM <deea.fruja@yahoo.com>
2024-04-24 16:06:01 +0300

    Connection of mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import cors from 'mongoose'
+import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
@@ -17,6 +17,27 @@ app.get('/', (req, res) => {
   res.send('Api is working')
 })
 
+//database connection
+mongoose.set('strictQuery', false)
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    console.log('MongoDB database is connected')
+  } catch (err) {
+    console.log('MongoDB database connection failed')
+  }
+}
+
+// middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
 app.listen(port, () => {
+  connectDB()
   console.log('Server is running on port' + port)
 })
