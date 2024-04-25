3deb74a7b3704f686e3af8855961859687b4966e
AndreeaFM <deea.fruja@yahoo.com>
2024-04-26 02:17:51 +0300

    register and login connection

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -3,6 +3,16 @@ import Doctor from '../models/DoctorSchema.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: '15d', //maybe 15d like it firstly put
+    }
+  )
+}
+
 export const register = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body
 
@@ -57,6 +67,51 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
+  const { email } = req.body
+
   try {
-  } catch (error) {}
+    let user = null
+
+    const patient = await User.findOne({ email })
+    const doctor = await Doctor.findOne({ email })
+
+    if (patient) {
+      user = patient
+    }
+    if (doctor) {
+      user = doctor
+    }
+
+    // check if user exist or not
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    // compare password
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    )
+
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ status: false, message: 'Invalid credentials' })
+    }
+
+    // get token
+    const token = generateToken(user)
+
+    const { password, role, appointments, ...rest } = user._doc
+
+    res.status(200).json({
+      status: true,
+      message: 'Successfully login',
+      token,
+      data: { ...rest },
+      role,
+    })
+  } catch (err) {
+    res.status(500).json({ status: false, message: 'Failed to login' })
+  }
 }
