c6c221973c98d61f497ed3639c489e5d0c185ec2
AndreeaFM <deea.fruja@yahoo.com>
2024-04-26 22:26:38 +0300

    doctors controller

---
+++ backend/Controllers/doctorController.js
@@ -0,0 +1,82 @@
+import Doctor from '../models/DoctorSchema.js'
+
+export const updateDoctor = async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const updatedDoctor = await Doctor.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    )
+
+    res.status(200).json({
+      success: true,
+      message: 'Successfully updated',
+      data: updatedDoctor,
+    })
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Failed to update' })
+  }
+}
+
+export const deleteDoctor = async (req, res) => {
+  const id = req.params.id
+
+  try {
+    await Doctor.findByIdAndDelete(id)
+
+    res.status(200).json({
+      success: true,
+      message: 'Successfully deleted',
+    })
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Failed to delete' })
+  }
+}
+
+export const getSingleDoctor = async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const doctor = await Doctor.findById(id).select('-password')
+
+    res.status(200).json({
+      success: true,
+      message: 'Doctor found',
+      data: doctor,
+    })
+  } catch (err) {
+    res.status(404).json({ success: false, message: 'No Doctor found' })
+  }
+}
+
+export const getAllDoctor = async (req, res) => {
+  try {
+    const { query } = req.query //used to filter data based on specific criteria
+    let doctors
+
+    if (query) {
+      doctors = await Doctor.find({
+        isApproved: 'approved',
+        $or: [
+          { name: { $regex: query, $options: 'i' } },
+          { specialization: { $regex: query, $options: 'i' } },
+        ], //use for find doctors by name or specialization, i means case sensitive searching
+      }).select('-password')
+    } else {
+      doctors = await Doctor.find({ isApproved: 'approved' }).select(
+        '-password'
+      ) //exlude the password field for sensitive data
+      // by using isApproved: 'approved' you cannot see all the data in postman
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Doctors found',
+      data: doctors,
+    })
+  } catch (err) {
+    res.status(404).json({ success: false, message: 'Not found' })
+  }
+}
