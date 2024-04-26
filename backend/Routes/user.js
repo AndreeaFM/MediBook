b0747c40cb6a9e96b354c73746dbb158e1d8039c
AndreeaFM <deea.fruja@yahoo.com>
2024-04-26 23:26:27 +0300

    authorization

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,9 +6,11 @@ import {
   getSingleUser,
 } from '../Controllers/userController.js'
 
+import { authenticate, restrict } from '../auth/verifyToken.js'
+
 const router = express.Router()
 
-router.get('/:id', getSingleUser)
+router.get('/:id', authenticate, restrict(['admin']), getSingleUser)
 router.get('/', getAllUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
