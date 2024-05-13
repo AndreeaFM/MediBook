4ce8967c69273becba0b1bddb43dd990134cdd3e
AndreeaFM <deea.fruja@yahoo.com>
2024-05-13 21:47:22 +0300

    review updated

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -14,7 +14,7 @@ const router = express.Router()
 
 // nested route
 
-router.use('/doctorId/reviews', reviewRouter)
+router.use('/:doctorId/reviews', reviewRouter)
 
 router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctor)
--- backend/Routes/review.js
+++ backend/Routes/review.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getAllReviews, createReview } from '../Controllers/reviewController.js'
-import { authenticate, restrict } from '../auth/verifyToken.js'
+import { authenticate, restrict } from './../auth/verifyToken.js'
 
 const router = express.Router({ mergeParams: true }) //to ensure that paramaters such as doctor id are accesible in nested route
 
